f2e420a9e60e2450799f60085ce912bff536051e
Garrett Wollman <wollman@csail.mit.edu>
2016-03-08 17:22:11 -0500

    ntp: do not depend on $::localbase
    When on FreeBSD and using packaged ntpd, just hardcode the
    path to ntpd rather than depending on the $::localbase fact,
    which is not universally defined (and in fact seems to have
    disappeared in our own installation).

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,7 +40,7 @@ class ntp (
   if $::osfamily == 'FreeBSD' and $managed_package {
     file_line { 'ntpd_program':
       path	=> '/etc/rc.conf',
-      line	=> "ntpd_program=\"${::localbase}/sbin/ntpd\"",
+      line	=> "ntpd_program=\"/usr/local/sbin/ntpd\"",
       match	=> '^ntpd_program=',
       before	=> [ Service['ntp'] ],
       require	=> [ Package['ntp'] ],
